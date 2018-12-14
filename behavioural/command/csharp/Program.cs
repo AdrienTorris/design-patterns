4345793541de33c80fdbfcb17069b089ca0e80e7
Adrien Torris <adrientorris@gmail.com>
2018-12-14 09:07:25 +0100

    Command pattern real world example (calculator)

--- behavioural/command/csharp/Program.cs
+++ behavioural/command/csharp/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace DesignPatterns.Behavioural.Command
+namespace DesignPatterns.Behavioural.Command
 {
     using System;
     using DesignPatterns.Behavioural.Command.RealWorldExample;
@@ -9,6 +9,8 @@
         static void Main(string[] args)
         {
             StructuralExampleMain();
+			
+			RealWorlExampleMain();
 
             Console.Read();
         }
@@ -17,9 +19,14 @@
         {
             Console.WriteLine("Structural example");
 			
+        }
 
+        static void RealWorlExampleMain()
+        {
             Console.WriteLine("Real world example");
 			
+			InteractiveShell interactiveShell = new InteractiveShell();
+			interactiveShell.Run();
         }
     }
 }
@@ -34,10 +41,11 @@ namespace DesignPatterns.Behavioural.Command.Structural
 namespace DesignPatterns.Behavioural.Command.RealWorldExample
 {
     using System;
+	using System.Collections.Generic;
 
     public sealed class MathLib
 	{
-		protected int currentValue = 0;
+		private int currentValue = 0;
 		
 		public int CurrentValue 
 		{
@@ -62,9 +70,11 @@ namespace DesignPatterns.Behavioural.Command.RealWorldExample
 	
 	public abstract class AbstractOperation
 	{
+		// Execute the operation
 		public abstract void Execute();
 		
-		public abstract AbstractOperation Inverse( get; }
+		// Gets the inverse of the operation
+		public abstract AbstractOperation Inverse { get; }
 	}
 	
 	public class Operation : AbstractOperation
@@ -111,8 +121,78 @@ namespace DesignPatterns.Behavioural.Command.RealWorldExample
 				{
 					case "+":
 						return new Operation(this.mathlib, "-", this.argument);
+					case "-":
+						return new Operation(this.mathlib, "+", this.argument);
+					case "*":
+						return new Operation(this.mathlib, "/", this.argument);
+					case "/":
+						return new Operation(this.mathlib, "*", this.argument);
+					default:
+						throw new InvalidOperationException("Invalid math operation");
 				}
 			}
+		}
+	}
+	
+	public class InteractiveShell
+	{
+		public void Run()
+		{
+			MathLib mathLib = new MathLib();
+			
+			Calculator calculator = new Calculator();
+			
+			Console.WriteLine("Please input a math operation:");
+			Console.WriteLine("     + <number>: add number");
+			Console.WriteLine("     - <number>: substract number");
+			Console.WriteLine("     * <number>: multiply number");
+			Console.WriteLine("     / <number>: divide number");
+			Console.WriteLine("     ~ <steps>: undo steps");
+			Console.WriteLine("     q: quit");
+			
+			string input = Console.ReadLine();
+			while(input!="q")
+			{
+				if(input.Length >= 2)
+				{
+					string op = input.Substring(0,1);
+					int arg = int.Parse(input.Substring(1));
+					
+					// Handle undo command
+					if(op=="~")
+					{
+						for(int i=0;i<arg;i++)
+						{
+							calculator.Undo();
+						}
+					}else{
+						AbstractOperation operation = new Operation(mathLib, op, arg);
+						
+						calculator.Invoke(operation);
+					}
+				}
+				
+				Console.WriteLine("Result: {0}", mathLib.CurrentValue);
+				input = Console.ReadLine();
+			}
+		}
+	}
+	
+	public class Calculator
+	{
+		protected Stack<AbstractOperation> undoStack = new Stack<AbstractOperation>();
+		
+		public void Invoke(AbstractOperation operation)
+		{
+			operation.Execute();
+			undoStack.Push(operation);
+		}
+		
+		// Undo the last operation
+		public void Undo()
+		{
+			var operation = undoStack.Pop();
+			operation.Inverse.Execute();
 		}
 	}
 }
